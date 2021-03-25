8ea17a6a6213bc3fcf3ec25e18a76ab73d59910a
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-03-26 01:40:39 +0900

    4-4

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+// Route::get('/', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login.index');
+// Route::get('/user', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('user.register');
+Route::get('/memo', function () {
+    return view("memo");
+})->name('memo.index');
