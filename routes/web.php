5731f11eba05d88eead095822eef5973219f33f3
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-03-26 19:08:33 +0900

    Revert "finished all part"
    This reverts commit 4457afe68ce63c2488ece9d0d42fcb4831a741db.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\MemoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,7 +25,6 @@ require __DIR__ . '/auth.php';
 
 // Route::get('/', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login.index');
 // Route::get('/user', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('user.register');
-
-Route::get('/memo', [MemoController::class, 'index'])->name('memo.index');
-
-Route::get('/memo/add', [MemoController::class, 'add'])->name('memo.add');
+Route::get('/memo', function () {
+    return view("memo");
+})->name('memo.index');
