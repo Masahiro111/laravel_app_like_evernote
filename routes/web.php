c3b7745ff1556ff65f59b4451dc01f9d73e21d18
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-04-28 02:00:16 +0900

    Revert "4-4"
    This reverts commit 8ea17a6a6213bc3fcf3ec25e18a76ab73d59910a.

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,4 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__ . '/auth.php';
-
-// Route::get('/', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login.index');
-// Route::get('/user', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('user.register');
-Route::get('/memo', function () {
-    return view("memo");
-})->name('memo.index');
+require __DIR__.'/auth.php';
