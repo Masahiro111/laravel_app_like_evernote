02a018a29371c0152ed3341fdb7a24902f1fae50
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-04-28 01:59:36 +0900

    Revert "finished all part"
    This reverts commit 4457afe68ce63c2488ece9d0d42fcb4831a741db.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,14 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/', [ProductController::class, 'index'])->name('product.index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
+
+// Route::get('/', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login.index');
+// Route::get('/user', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('user.register');
+Route::get('/memo', function () {
+    return view("memo");
+})->name('memo.index');
