2a903ed457d5278f503ffa4b68642626a161c6cd
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-05-02 00:29:51 +0900

    finished part 3-3

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ProductController::class, 'index'])
     ->name('product.index');
 
+Route::get('/product/{id}', [ProductController::class, 'show'])
+    ->name('product.show');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
